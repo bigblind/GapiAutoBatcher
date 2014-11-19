dbbe1d1fd3eb2457409e08800e985d064a425234
Frederik Creemers <frederikcreemers@gmail.com>
2014-11-19 03:15:29 +0100

    implemented GapiAutoBatcher.call

--- GapiAutoBatcher.js
+++ GapiAutoBatcher.js
@@ -1,4 +1,6 @@
 GapiAutoBatcher = function(config){
+
+
   if(this === window){
     return new GapiAutoBatcher(config);
   }
@@ -6,12 +8,44 @@ GapiAutoBatcher = function(config){
   config = config || {};
   this.config = {};
 
-  // set defaults
+
+  // Make sure all configuration options have a value
   for (var option in GapiAutoBatcher.defaults){
     if (GapiAutoBatcher.defaults.hasOwnProperty(option)){
       this.config[option] = config[option] || GapiAutoBatcher.defaults[option];
     }
   }
+
+  var batch = null;
+  var timeout = null;
+  var started = null;
+  var callBatch = function(){
+    batch.execute();
+    batch = null;
+  }
+
+  this.call = function(request){
+    if (batch == null){
+      started = Date.now()
+      batch = gapi.client.newBatch();
+    }
+
+    batch.add(request);
+
+    if (timeout){
+      clearTimeout(timeout);
+    }
+    var delay = this.config.batchInterval;
+    var elapsed = Date.now() - started;
+    console.log("elapsed: " + elapsed);
+    if (elapsed + delay > this.config.maxWait){
+      console.log("delay too long: " + elapsed + " + " + delay + " > " + this.config.maxWait);
+      delay = this.config.maxWait - elapsed;
+      console.log("new delay " + delay);
+    }
+    timeout = setTimeout(callBatch, delay);
+  }
+
 }
 
 
--- tests.js
+++ tests.js
@@ -1,8 +1,31 @@
+MockGapi = function(){
+  var functions = [];
+  this.add = function(f){
+    console.log("added request to batch.");
+    functions.push(f)
+  }
+  this.execute = function(){
+    console.log("execute called")
+    for (var i=0; i<functions.length; i++){
+      functions[i]();
+      console.log(functions[i])
+    }
+  }
+}
+
+setupGapi = function(){
+  gapi = {};
+  gapi.client = {};
+  gapi.client.newBatch = sinon.spy(function(){
+    return new MockGapi()
+  });
+}
+
 describe('GapiAutoBatcher', function(){
   describe('constructor', function(){
 
     it('should return a new instance when called without "new".', function(){
-      expect(GapiAutoBatcher()).to.deep.equal(new GapiAutoBatcher());
+      expect(GapiAutoBatcher()).to.be.instanceof(GapiAutoBatcher);
     });
 
     it("should use the default option when an option is not specified", function(){
@@ -13,8 +36,110 @@ describe('GapiAutoBatcher', function(){
       var batchInterval = 42;
       expect(GapiAutoBatcher({batchInterval: batchInterval}).config.batchInterval)
           .to.equal(batchInterval);
+    });
+
+  });
+
+  describe("call", function(){
+    it("should execute a request passed to call() after config.batchInterval, if " +
+        "no other request is passed in within  that time.", function(){
+      setupGapi();
+      var interval = 30;
+      var batcher = new GapiAutoBatcher({batchInterval: interval});
+      var clock = sinon.useFakeTimers();
+      var request = sinon.spy();
+
+      batcher.call(request);
+      clock.tick(interval);
+
+      expect(request.callCount).to.equal(1);
+
+      clock.restore();
+    });
+
+    it("should not execute a request passed in to call() before the necessary time has elapsed", function(){
+      setupGapi();
+      var interval = 30;
+      var batcher = new GapiAutoBatcher({batchInterval: interval});
+      var clock = sinon.useFakeTimers();
+
+      var request = sinon.spy();
+
+      clock.restore();
+      expect(request.callCount).to.equal(0);
+    });
+
+    it("should extend the interval by batchInterval if a new request is added.", function(){
+      setupGapi();
+      var interval = 30;
+      var batcher = new GapiAutoBatcher({batchInterval: interval});
+      var clock = sinon.useFakeTimers();
+      var request1 = sinon.spy();
+      var request2 = sinon.spy();
+
+      batcher.call(request1);
+      clock.tick(29);
+      batcher.call(request2);
+      clock.tick(31);
+
+      expect(request1.callCount).to.equal(1);
+      expect(request2.callCount).to.equal(1);
+      clock.restore();
+    });
+
+    it("should not execute the batch before the extended timer has run out", function(){
+      setupGapi();
+      var interval = 30;
+      var batcher = new GapiAutoBatcher({batchInterval: interval});
+      var clock = sinon.useFakeTimers();
+      var request1 = sinon.spy();
+      var request2 = sinon.spy();
+
+      batcher.call(request1);
+      clock.tick(29);
+      batcher.call(request2);
+      clock.tick(5);
+
+      expect(request1.callCount).to.equal(0);
+      expect(request2.callCount).to.equal(0);
+      clock.restore();
+    });
+
+    it("should only extend the timeout upto confix.maxWait", function(){
+      setupGapi();
+      var interval = 30, maxWait = 50;
+      var batcher = new GapiAutoBatcher({batchInterval: interval, maxWait: maxWait});
+      var clock = sinon.useFakeTimers();
+      var request1 = sinon.spy();
+      var request2 = sinon.spy();
+
+      batcher.call(request1);
+      clock.tick(29);
+      batcher.call(request2);
+      clock.tick(21);
+
+      expect(request1.callCount).to.equal(1);
+      expect(request2.callCount).to.equal(1);
+      clock.restore();
     })
 
-  })
+    it("should start a new batch after the previous batch has been executed", function(){
+      setupGapi();
+      var interval = 30, maxWait = 50;
+      var batcher = new GapiAutoBatcher({batchInterval: interval, maxWait: maxWait});
+      var clock = sinon.useFakeTimers();
+      var request1 = sinon.spy();
+      var request2 = sinon.spy();
 
+      batcher.call(request1);
+      clock.tick(31);
+      batcher.call(request2);
+      clock.tick(31);
+
+      expect(request1.callCount).to.equal(1);
+      expect(request2.callCount).to.equal(1);
+      clock.restore();
+    })
+
+  });
 })
